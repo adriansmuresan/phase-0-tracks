3b92b2b40b34b603af3077634eb5058133956e48
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-18 14:21:24 -0500

    Add driver code

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -1,6 +1,10 @@
 # 6.3 Attributes
 class Santa
-  def initialize
+  def initialize(gender,ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
     puts "Initializing Santa instance ..."
   end
 
@@ -12,7 +16,30 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 end
+# Driver code
+# santa = Santa.new
+# santa.speak
+# santa.eat_milk_and_cookies("snickerdoodle")
 
-david = Santa.new
-david.speak
-david.eat_milk_and_cookies("snickerdoodle")
+# santa = Santa.new("agender", "latino")
+# santa.speak
+# santa = Santa.new("agender", "black")
+# santa.speak
+# santa = Santa.new("female", "Latino")
+# santa.speak
+# santa = Santa.new("bigender", "white")
+# santa.speak
+# santa = Santa.new("male", "Japanese")
+# santa.speak
+# santa.eat_milk_and_cookies("gingerbread")
+
+ santas = []
+
+ santas << Santa.new("agender", "black")
+ santas << Santa.new("female", "Latino")
+ santas << Santa.new("bigender", "white")
+ santas << Santa.new("male", "Japanese")
+ santas << Santa.new("female", "prefer not to say")
+ santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+ santas << Santa.new("N/A", "N/A")
+ p santas
