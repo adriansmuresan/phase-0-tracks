5b94ece8301203c5c4b897988602a774fb5918a2
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-19 00:16:02 -0500

    Add mixin to a module

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,13 +1,35 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!" + ":)"
+#     end
+#  end
+
+
+# p Shout.yell_angrily("Uffff")
+# p Shout.yelling_happily("Yuhuuuu")
+
+# Release 3
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell(words)
+		puts "He was yelling from the top of his lungs: #{words} "
 	end
+end
 
-	def self.yelling_happily(words)
-		words + "!!" + ":)"
-    end
- end
+class Yell_angrily
+	include Shout
+end
 
+class Yelling_happily
+	include Shout
+end
 
-p Shout.yell_angrily("Uffff")
-p Shout.yelling_happily("Yuhuuuu")
+happy = Yelling_happily.new
+happy.yell("I made it!!!")
+
+angry = Yell_angrily.new
+angry.yell("You will regret this!!!")
