0bb6d4796ca00c88aa6f7ab50a65aeaf4faf9e89
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-18 21:27:57 -0500

    Add pseudocode

--- ruby/solo2.rb
+++ ruby/solo2.rb
@@ -1,1 +1,21 @@
 # 6.4 Solo Challenge: Designing and Implementing a Class
+
+# Design (as opposed to actually building) a class representing anything you like.
+# (Please make it something we have at least heard of.)
+# Whether you want to build a fire-breathing dragon, a dress, or a soccer ball, we hereby grant you the freedom to implement it.
+# No matter what you design, your class must have:
+# at least three attributes (using at least two data types) whose values will vary from instance to instance (example: age)
+# at least three methods, at least one of which takes an argument
+# You're welcome to be creative about these: a dress can have a swish method, a soccer ball can have an inflatedness attribute,
+# and ... we hope it's obvious what a fire-breathing dragon can do, but if not, you'll figure it out.
+
+# Class Movies
+# Attributes:
+   # age
+   # genres: action, adventure, drama, romance, comedy, westerns, thrillers
+   # name
+   # popcorn
+# Methods
+   # A method that verifies the age
+   # A method that chouses a random genres
+   # A method that asks the user if he whants popcorn   
