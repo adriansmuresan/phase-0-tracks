821439f43e0e22d3b3e6ca29d2f86c108a2873a9
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-18 23:58:20 -0500

    Add a simple module

--- ruby/shout2.rb
+++ ruby/shout2.rb
@@ -1,2 +1,13 @@
 module Shout
-end
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yelling_happily(words)
+		words + "!!" + ":)"
+    end
+ end
+
+
+p Shout.yell_angrily("Uffff")
+p Shout.yelling_happily("Yuhuuuu")
