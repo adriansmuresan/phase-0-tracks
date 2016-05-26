abcd895f3b236d94ff66a6ed92acbb3a43470185
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-25 19:09:05 -0500

    Add a Car function(constructor), build an object and play with arrays.

---
+++ js/data_structures2.js
@@ -0,0 +1,32 @@
+var colors = ["blue", "green", "orange", "white"];
+var horses = ["Ed", "Joe", "Jeff", "Bob"];
+
+colors.push("purple")
+
+horses.push("Mike")
+// console.log(horses);
+
+var horseObject = {};
+for(var x = 0;x < horses.length; x++){
+  horseObject[horses[x]] = colors[x];
+}
+
+console.log(horseObject)
+
+function Car(model,year,color) {
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.drive = function() {console.log("Vrooommmm!!!");};
+}
+
+var newCar = new Car ("Bmw", "2015", "Black");
+console.log(newCar);
+console.log("This car is a " + newCar.year + ' ' + newCar.color + ' ' + newCar.model + '.');
+newCar.drive();
+
+var otherCar = new Car ("Mercedes", "2008", "Red");
+console.log(otherCar);
+console.log("This car is a " + otherCar.year + ' ' + otherCar.color +' ' + otherCar.model + '.');
+otherCar.drive();
+
