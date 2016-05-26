da99eb8e37f30696739600d3ab6fa19742285ac9
Adrian S MUresan <adriansmuresan@gmail.com>
2016-05-25 19:15:57 -0500

    Add loop through the keys and values of the object

--- js/data_structures2.js
+++ js/data_structures2.js
@@ -24,9 +24,17 @@ var newCar = new Car ("Bmw", "2015", "Black");
 console.log(newCar);
 console.log("This car is a " + newCar.year + ' ' + newCar.color + ' ' + newCar.model + '.');
 newCar.drive();
+console.log("------------")
 
 var otherCar = new Car ("Mercedes", "2008", "Red");
 console.log(otherCar);
 console.log("This car is a " + otherCar.year + ' ' + otherCar.color +' ' + otherCar.model + '.');
 otherCar.drive();
+
+console.log("-------------")
+// add loop through the keys and values 
+for (var key in otherCar) {
+	console.log("The " + key + " has this value " + otherCar[key]);
+}
+
 
