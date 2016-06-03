54a36315d68fe0e2300877a16e9bd0b0c2e95dc9
Adrian S MUresan <adriansmuresan@gmail.com>
2016-06-02 21:13:29 -0500

    Create a database called puppyes.db

--- databases/puppy_maker/puppy_maker.rb
+++ databases/puppy_maker/puppy_maker.rb
@@ -1,3 +1,27 @@
 # Operation puppy explosion!
 
 # talk about Object Relational Mapping (ORM)
+
+# require gems
+require 'sqlite3'
+require 'faker'
+
+# create SQLite3 database
+db = SQLite3::Database.new("puppyes.db")
+
+# learn about fancy string delimiters
+
+# create a puppyes table (if it's not there already)
+
+# add a test puppy
+# db.execute("INSERT INTO puppyes (name, age) VALUES ('Joe', 5)")
+
+# add LOOOOTS of puppyes!
+# so. many. puppyes. 
+#PuppyExplosion
+
+# explore ORM by retrieving data
+# puppyes = db.execute("SELECT * FROM puppyes")
+# puppyes.each do |puppy|
+#  puts "#{puppy['name']} is #{puppy['age']}"
+# end
