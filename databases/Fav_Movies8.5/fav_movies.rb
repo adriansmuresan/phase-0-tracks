38b940e5d4f99d0b42bf69de01109a7cd4f22113
Adrian S MUresan <adriansmuresan@gmail.com>
2016-06-05 14:07:20 -0500

    Add a table to the database

--- databases/Fav_Movies8.5/fav_movies.rb
+++ databases/Fav_Movies8.5/fav_movies.rb
@@ -1,6 +1,24 @@
-# Create a program to add your personal afvorite movies with rating and comments
+# 8.5 Combining SQL and Ruby
+
+# Create a program to add your personal favorite movies with rating and comments
 
 require 'sqlite3'
 require'faker'
 
 db = SQLite3::Database.new("movies.db")
+db.results_as_hash = {}
+
+create_table_cmd = <<-SQL 
+CREATE TABLE IF NOT EXISTS movies (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255),
+	rating INT,
+	comments VARCHAR(255)
+	)
+SQL
+
+# create a movies table (if it's not there already)
+db.execute(create_table_cmd)
+
+# add test movie 
+# db.execute("INSERT INTO movies (name, rating, comments) VALUES ('Deadpool', 4, 'Highly entertaining')")
